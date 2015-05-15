Fix timeline tab behavior. Add timeline variation operation selector to context menu. Fix remesh behavior when no alpha is found. Fix polygon masking for new coordinate system. Add application-level main.py.

hipies/timeline.py
@@ -48,37 +48,43 @@ class timelinetab(viewer.imageTab):
         self.paths = paths
         self.experiment = experiment
         self.parentwindow = parentwindow
-        self.variation = np.zeros(self.paths.__len__() - 1)
-        self.scan()
-        self.plotvariation()
+        self.setvariationmode(0)
         self.gotomax()
 
 
     def scan(self):
-        operation = lambda c, p: np.sum(np.square(c.p))
-        operation2 = lambda c, p: np.sum(np.abs(c - p))
-        operation3 = lambda c, p: np.sum(c)
+        self.variation = np.zeros(self.paths.__len__() - 1)
+        operations = [lambda c, p: np.sum(np.square(c - p) / p),  # Chi squared
+                      lambda c, p: np.sum(np.abs(c - p)),  # Absolute difference
+                      lambda c, p: np.sum(np.abs(c - p) / p),  # Norm. absolute difference
+                      lambda c, p: np.sum(c)]  # Sum intensity
         # print(':P')
         #print self.paths
 
         #get the first frame's profile
-        prev, paras = pipeline.loader.loadpath(self.paths[0])
+        prev, _ = pipeline.loader.loadpath(self.paths[0])
         for i in range(self.paths.__len__() - 1):
             #print i, self.paths[i]
-            curr, paras = pipeline.loader.loadpath(self.paths[i + 1])
+            curr, _ = pipeline.loader.loadpath(self.paths[i + 1])
             if curr is None:
                 self.variation[i] = None
                 continue
 
             #print curr, prev,'\n'
 
-            self.variation[i] = operation2(curr, prev)
-            prev = curr
+            self.variation[i] = operations[self.operationindex](curr, prev)
+            prev = curr.copy()
             # print self.variation
+
+    def setvariationmode(self, index):
+        self.operationindex = index
+        self.scan()
+        self.plotvariation()
 
 
     def plotvariation(self):
         self.parentwindow.timeline.clear()
+        self.parentwindow.timeline.enableAutoScale()
         self.parentwindow.timeruler = pg.InfiniteLine(pen=pg.mkPen('#FFA500', width=3), movable=True)
         self.parentwindow.timeline.addItem(self.parentwindow.timeruler)
         self.parentwindow.timeruler.setBounds([0, self.variation.__len__() - 1])

hipies/viewer.py
@@ -603,7 +603,7 @@ class imageTab(QtGui.QWidget):
             toppad = boundrect.y()
 
             # Pad the mask so it has the same shape as the image
-            maskedarea = np.pad(maskedarea, ((int(leftpad), 0), (int(toppad), 0)), mode='constant').T
+            maskedarea = np.pad(maskedarea, ((int(leftpad), 0), (int(toppad), 0)), mode='constant')
             maskedarea = np.pad(maskedarea, (
                 (0, self.imgdata.shape[0] - maskedarea.shape[0]), (0, self.imgdata.shape[1] - maskedarea.shape[1])),
                                 mode='constant')

pipeline/integration.py
@@ -19,11 +19,13 @@ def radialintegrate(imgdata, experiment, mask=None, cut=None):
     # else:
     #    mask = self.config.maskingmat
 
+    invmask=1-mask
+
     #mask data
-    data = imgdata * (1 - mask)
+    data = imgdata * (invmask)
 
     if cut is not None:
-        mask *= cut
+        invmask *= cut
         data *= cut
 
     #calculate data radial profile
@@ -32,7 +34,7 @@ def radialintegrate(imgdata, experiment, mask=None, cut=None):
     r = r.astype(np.int)
 
     tbin = np.bincount(r.ravel(), data.ravel())
-    nr = np.bincount(r.ravel(), (1 - mask).ravel())
+    nr = np.bincount(r.ravel(), (invmask).ravel())
     radialprofile = tbin / nr
 
     q = np.arange(radialprofile.shape[0])

pipeline/remesh.py
@@ -76,7 +76,7 @@ def remesh(img, filename, geometry):
     # read incident angle
     if paras is None:
         print "Failed to read incident angle"
-        return img
+        return np.rot90(img,3)
     alphai = paras["Sample Alpha Stage"] / 360.0 * 2 * np.pi
 
     nz, ny = img.shape