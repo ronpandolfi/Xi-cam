f916cfe042d1ae849fd8bf86541affd2be3ed618
austinblair <ABlair@lbl.gov>
2015-07-08 15:21:43 -0700

    Made changes in order to fix the useage of the buttons in QT designer,and improve the flow of button layout.

--- hipies/rmc.py
+++ hipies/rmc.py
@@ -13,7 +13,18 @@ class gui():
         self.ui.rmcaddfiles.clicked.connect(self.addfiles)
         self.ui.rmcremovefiles.clicked.connect(self.removefiles)
         self.ui.rmcExecute.clicked.connect(self.execute)
+        self.ui.rmcAddloadingfactor.clicked.connect(self.addloadingfactor)
+        self.ui.rmcSubtractloadingfactor.clicked.connect(self.subtractloadingfactor)
+        self.ui.rmcopen.clicked.connect(self.open)
+        self.ui.rmcreset.clicked.connect(self.reset)
 
+
+    def open(self):
+        Outputdirectory= QtGui.QFileDialog.getExistingDirectory(self.ui,"Select an Output directory")
+        self.ui.rmcoutput.setText(Outputdirectory)
+
+    def reset(self):
+        self.ui.rmcoutput.setText("")
 
     def showrmc(self):
         """
@@ -28,13 +39,19 @@ class gui():
         self.ui.rmcinputpaths.addItems(paths)
 
     def removefiles(self):
-        a = self.ui.rmcinputpaths
-        '''
-        :type a : PySide.QtGui.QListWidget
-        '''
-        a.QListWidget.selectedItems()
         for index in self.ui.rmcinputpaths.selectedIndexes():
-            item = self.ui.rmcinputpaths.takeItem(index)
+            item = self.ui.rmcinputpaths.takeItem(index.row())
+            item = None
+
+    def addloadingfactor(self):
+        loadingfactor,_=QtGui.QInputDialog.getDouble(self.ui, "Loading Factor", "Enter a loading factor:", value=0, minValue=-0, maxValue=1000, decimals=3 )
+        newItem = QtGui.QListWidgetItem()
+        newItem.setText(str(loadingfactor))
+        self.ui.rmcLoadingfactors.addItem(newItem)
+
+    def subtractloadingfactor(self):
+        for index in self.ui.rmcLoadingfactors.selectedIndexes():
+            item = self.ui.rmcLoadingfactors.takeItem(index.row())
             item = None
 
     def execute(self):
@@ -42,16 +59,17 @@ class gui():
         steps = self.ui.rmcSteps.value()
         scalefactor = self.ui.rmcScalefactor.value()
         modlestartsize = self.ui.rmcModlestartsize.value()
-        lodingfactors_1 = self.ui.rmcLoadingfactors_1.value()
-        lodingfactors_2 = self.ui.rmcLoadingfactors_2.value()
-        lodingfactors_3 = self.ui.rmcLoadingfactors_3.value()
+        #lodingfactors = self.ui.rmcLoadingfactors.
+
+
+
         rip=self.ui.rmcinputpaths
         inputpaths = [rip.item(index).text() for index in xrange(rip.count())]
 
         d = {'hipRMCInput': {'instrumentation': {'inputimage': inputpaths[0],
                                              'imagesize': loader.loadimage(inputpaths[0]).shape,
                                              'numtiles': tiles,
-                                             'loadingfactors': [lodingfactors_1, lodingfactors_2, lodingfactors_3 ]},
+                                             'loadingfactors': [lodingfactors ]},
                          'computation': {'runname': "test",
                                          'modelstartsize': [modlestartsize, modlestartsize],
                                          'numstepsfactor': steps,
