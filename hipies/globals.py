edb4be6e7d2e287187e6fb78fbd3827788ba3727
Ronald Pandolfi <ronpandolfi@gmail.com>
2016-01-12 14:47:37 -0800

    add globals module.

---
+++ hipies/globals.py
@@ -0,0 +1,14 @@
+import multiprocessing
+from collections import OrderedDict
+
+LUT = None
+LUTlevels = None
+LUTstate = None
+plugins = OrderedDict()
+pool = None
+
+
+def load():
+    global pool
+    multiprocessing.freeze_support()
+    pool = multiprocessing.Pool()
