3c639ff2c9778bd84370ef481d331165fdf3ee42
ronpandolfi <ronpandolfi@gmail.com>
2016-03-10 14:48:19 -0800

    Fix hipgisaxs lattice display

--- xicam/plugins/hipgisaxs/latvec.py
+++ xicam/plugins/hipgisaxs/latvec.py
@@ -1,6 +1,8 @@
 import numpy as np
 import itertools
 
+latvecmaxr=4
+latvecmaxz=2
 
 def reciprocalvectors(a, b, c, order=2):
     V = abs(np.dot(a, np.cross(b, c)))
@@ -13,16 +15,20 @@ def reciprocalvectors(a, b, c, order=2):
     return vecs
 
 
-def latticevectors(a, b, c, order=2):
+def latticevectors(a, b, c, zoffset, order=2):
     mi = np.vstack([a, b, c])
     combs = itertools.product(range(-order, order + 1), repeat=3)
     vecs = [np.sum((mi.T * np.array(comb)).T, axis=0) for comb in combs]
-    vecs = [vec for vec in vecs if vec[2] >= 0]
+    vecs = [zoffsetvec(vec,zoffset) for vec in vecs if vec[2] >= 0 and np.sqrt(vec[0]**2+vec[1]**2)<=latvecmaxr and vec[2]<=latvecmaxz]
     return vecs
 
+def zoffsetvec(vec,zoffset=0):
+    vec[2]=vec[2]+zoffset
+    return vec
 
-def latticelines(a, b, c, order=2):
-    vecs = latticevectors(a, b, c, order)
+
+def latticelines(a, b, c, zoffset, order=2):
+    vecs = latticevectors(a, b, c, zoffset, order)
 
     lines = []
     for vec in vecs:
