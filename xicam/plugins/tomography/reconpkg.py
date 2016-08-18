72e08ff22d04ee04cfcf796d7d701af16bf37f04
ronpandolfi <ronpandolfi@gmail.com>
2016-08-18 13:37:20 -0700

    Switch tomopy to hard import

--- xicam/plugins/tomography/reconpkg.py
+++ xicam/plugins/tomography/reconpkg.py
@@ -1,8 +1,9 @@
 import importlib
 from pipeline import msg
+import tomopy
 
 # Packages to import
-PACKAGE_LIST =['tomopy', 'astra', 'dxchange']
+PACKAGE_LIST = ['astra', 'dxchange']
 
 # Dictionary with package names as keys and package objects as values
 packages = {}
@@ -14,8 +15,10 @@ for name in PACKAGE_LIST:
         packages[name] = package
         msg.logMessage('{} module loaded'.format(name), level=20)
     except ImportError as ex:
-        msg.logMessage('{} module not available'.format(name), level=30)  # 30 -> warning
-        raise ex
+        msg.logMessage('{} module not available'.format(name), level=30)  # 30 -> warning'
+
+# Tomopy is actually necessary, so its important that its a hard import
+packages['tomopy'] = tomopy
 
 # Add the extra functions
 import pipelinefunctions
