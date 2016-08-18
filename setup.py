c955a480d0d79a8ff5fd3423a12b6995575d0694
Dan Allan <dallan@bnl.gov>
2016-08-18 13:13:15 -0400

    BLD: Add deps to install_requires that are required to start app.

--- setup.py
+++ setup.py
@@ -72,7 +72,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy','scipy','cython','fabio','pyFAI','h5py','shiboken','PySide','PyQtGraph','QDarkStyle','nexusformat','Pillow','pyfits','PyOpenGL','PyYAML','qtconsole'],
+    install_requires=['numpy', 'scipy', 'cython', 'fabio', 'pyFAI', 'h5py', 
+                      'shiboken', 'PySide', 'PyQtGraph', 'QDarkStyle', 
+                      'nexusformat', 'Pillow', 'pyfits', 'PyOpenGL', 'PyYAML', 
+                      'qtconsole', 'tifffile', 'pysftp', 'requests']
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
