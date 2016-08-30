fccd7fadca39ed5f9a725aa9c0ab81c5e3d6dbd9
hparks <holdenparks@yahoo.com>
2016-08-30 14:49:51 -0700

    formatting and commenting/docstringing

--- xicam/plugins/viewerRMC.py
+++ xicam/plugins/viewerRMC.py
@@ -4,9 +4,7 @@ from xicam.plugins import base
 from xicam import config
 from pyqtgraph import parametertree as pt
 from fabio import tifimage
-# from PIL import Image
 from pipeline import loader, hig, msg
-#from hiprmc import hiprmc
 import pyqtgraph as pg
 import numpy as np
 import subprocess
@@ -24,7 +22,27 @@ Bugs:
 
 class plugin(base.plugin):
 
-    # name can be changed upon request
+    """
+    HipRMC plugin class - centers images, calls HipRMC as subprocess, then displays output
+
+
+    Attributes
+    ----------
+    centerwidget : QtGui.QTabWidget
+        Standard centerwidget overriding base.plugin. QTabWidget that holds instances of inOutviewer for images and
+        HipRMC output
+    threadWorker : threads.Worker
+        Contains qtCore.QThreadpool on which to run HipRMC jobs, and a queue to hold the jobs
+
+
+    Parameters
+    ----------
+    args
+        Additional arguments. Not really used
+    kwargs
+        Additional keyword arguments. Not really used
+    """
+
     name = "ViewerRMC"
 
     #center widget is something, rightwidget is none, and leftwidget inherits the default from parent base.plugin
@@ -41,14 +59,6 @@ class plugin(base.plugin):
         self.threadWorker.pool.setExpiryTimeout(1)
 
 
-        # DRAG-DROP
-        # self.centerwidget.setAcceptDrops(True)
-        # self.centerwidget.dragEnterEvent = self.dragEnterEvent
-        # self.centerwidget.dropEvent = self.dropEvent
-
-
-
-
         super(plugin, self).__init__(*args, **kwargs)
 
 
@@ -60,23 +70,6 @@ class plugin(base.plugin):
         view_widget.drawCameraLocation(view_widget.orig_view,view_widget.cameraLocation)
 
 
-    # Do I need these??
-
-    # def dropEvent(self, e):
-    #     for url in e.mimeData().urls():
-    #         if op_sys == 'Darwin':
-    #             fname = str(NSURL.URLWithString_(str(url.toString())).filePathURL().path())
-    #         else:
-    #             fname = str(url.toLocalFile())
-    #         if os.path.isfile(fname):
-    #             self.openfiles([fname])
-    #         e.accept()
-    #
-    # def dragEnterEvent(self, e):
-    #     print(e)
-    #     e.accept()
-
-
     def tabClose(self,index):
         self.centerwidget.widget(index).deleteLater()
 
@@ -84,6 +77,48 @@ class plugin(base.plugin):
 
 class inOutViewer(QtGui.QWidget, ):
     def __init__(self, paths, worker, parent=None):
+        """
+           Class that holds image to be processed by HipRMC, image after it has been centered, and HipRMC output
+
+           Attributes
+           ----------
+           emitter : threads.Emitter
+               Holds and emits a signal when HipRMc done processing
+           cameraLocation : tuple
+               2-tuple (x,y) of camera location on input image
+           rmcView : ximcam.RmcView.rncView
+               Timeline viewer which holds and displays HipRMC output
+           orig_image: np.array
+               Original input image
+           edited_image : np.array
+               Image with camera location adjusted to its center
+           orig_view : pyqtgraph.ImageView
+               Holds the original image
+           edited_view : pyqtgraph.ImageView
+               Holds the image after camera location adjustment
+           image_holder : QtGui.StackedWidget
+               Main widget of plugin. Holds original and edited images, as well as HipRMC output, in tabs
+           scatteringParams : pyqtgraph.parametertree
+               Occupies right side of main widget. Holds configparams
+           configparams : pyqtgraph.Parameter
+               Class held by scatteringParams which holds parameter values for HipRMC
+           output, err : str
+               Output and error from HipRMC subprocess call
+
+           Parameters
+           ----------
+           paths : str/list of str
+               Path to input dataset
+           worker: threads.Worker
+               Worker which queues up jobs and runs them on a QtCore.QThreadpool
+           parent : QtGui.QWidget
+               parent widget
+           args
+               Additional arguments
+           kwargs
+               Additional keyword arguments
+           """
+
 
         super(inOutViewer, self).__init__(parent=parent)
 
@@ -91,8 +126,11 @@ class inOutViewer(QtGui.QWidget, ):
 
         layout = QtGui.QHBoxLayout()
         self.cameraLocation = config.activeExperiment.center
+
+        # the next two will be filled as different functions are fun
         self.rmc_view= None
         self.edited_image = None
+
         self.worker = worker
 
 
@@ -149,11 +187,11 @@ class inOutViewer(QtGui.QWidget, ):
         centerButton.clicked.connect(self.center)
         runButton.clicked.connect(self.runRMC)
 
-        self.headings = QtGui.QTabBar(self)
-        self.headings.addTab('Original Image')
-        self.headings.addTab('Recentered Image')
-        self.headings.addTab('RMC Timeline')
-        self.headings.setShape(QtGui.QTabBar.TriangularSouth)
+        headings = QtGui.QTabBar(self)
+        headings.addTab('Original Image')
+        headings.addTab('Recentered Image')
+        headings.addTab('RMC Timeline')
+        headings.setShape(QtGui.QTabBar.TriangularSouth)
 
         self.drawROI(0,0,self.orig_image.shape[0],self.orig_image.shape[1], 'r',
                      self.orig_view.getImageItem().getViewBox())
@@ -164,20 +202,28 @@ class inOutViewer(QtGui.QWidget, ):
 
         sidelayout = QtGui.QVBoxLayout()
         sidelayout.addWidget(self.image_holder)
-        sidelayout.addWidget(self.headings)
+        sidelayout.addWidget(headings)
 
         layout.addLayout(sidelayout,10)
         layout.addLayout(sideWidgetFormat,4)
         self.setLayout(layout)
 
-        self.headings.currentChanged.connect(self.currentChanged)
-        self.image_holder.currentChanged.connect(self.headings.setCurrentIndex)
+        headings.currentChanged.connect(self.currentChanged)
+        self.image_holder.currentChanged.connect(headings.setCurrentIndex)
 
     def currentChanged(self,index):
+        """
+        Slot to recieve centerwidgets currentchanged signal when a new tab is selected
+        """
+
         self.image_holder.setCurrentIndex(index)
 
 
     def center(self):
+        """
+        Slot to receive signal when user requests a centered image. Performs centering and writes new image into
+        current working directory
+        """
 
         if self.edited_image is not None:
             self.image_holder.removeWidget(self.edited_view)
@@ -225,9 +271,20 @@ class inOutViewer(QtGui.QWidget, ):
         # this is a temporary fix for a problem: pushing a button changes tab back to first
         self.image_holder.setCurrentIndex(1)
 
-    def drawCameraLocation(self,imageView_item,location):
+    def drawCameraLocation(self,box,location):
+        """
+        Draws camera location as dot on image
 
-        cameraBox = imageView_item.getImageItem().getViewBox()
+        Parameters
+        ----------
+        box : pyqtgraph.ImageView
+            Class which holds the dot drawn by the function
+        location: tuple
+            2-tuple of camera location
+        """
+
+
+        cameraBox = box.getImageItem().getViewBox()
         cameraPlot = pg.ScatterPlotItem()
         cameraBox.addItem(cameraPlot)
         cameraPlot.setData([location[0]], [location[1]], pen=None,
@@ -237,6 +294,20 @@ class inOutViewer(QtGui.QWidget, ):
 
 
     def drawROI(self, xpos, ypos, xdim,ydim, color, view_box):
+        """
+        Draws camera location as dot on image
+
+        Parameters
+        ----------
+        xpos, ypos: int
+            Location of lower left corner of ROI
+        xdim, ydim : int
+            Size of ROI in x and y dimensions
+        color : str
+            Color of ROI
+        view_box : pyqtgraph.ImageView
+            Class which holds the ROI drawn by the function
+        """
 
         roi = pg.RectROI((xpos,ypos),(xdim,ydim),movable = False,removable=True)
         roi.setPen(color = color)
@@ -245,6 +316,12 @@ class inOutViewer(QtGui.QWidget, ):
 
 
     def runRMC(self):
+        """
+        Slot to receive signal when user requests HipRMC calculation. Writes hig file of parameter values and
+        calls HipRMC as subprocess
+        """
+
+
         msg.showMessage('Running RMC for centered version of {}'.format(self.path), timeout=0)
 
         if self.rmc_view is not None:
@@ -290,6 +367,9 @@ class inOutViewer(QtGui.QWidget, ):
 
 
     def run_RMCthread(self):
+        """
+        Slot to receive signal to run HipRMC as subprocess on background thread
+        """
 
         proc = subprocess.Popen(['./hiprmc/bin/hiprmc', self.hig_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.output, self.err = proc.communicate()
@@ -297,7 +377,9 @@ class inOutViewer(QtGui.QWidget, ):
 
     @QtCore.Slot()
     def RMC_done(self):
-
+        """
+        Slot to receive signal when HipRMC calculation is done. Emits signal to main thread to load output
+        """
         run_time = time.time() - self.start_time
         msg.showMessage('HipRMC complete. Run time: {:.2f} s'.format(run_time))
         self.emitter.sigFinished.emit()
@@ -305,7 +387,10 @@ class inOutViewer(QtGui.QWidget, ):
 
     @QtCore.Slot()
     def write_and_display(self):
-
+        """
+        Slot. Connected to RMC_done slot function. Loads images in rmc_viewer and writes HipRMC text output
+        as txt file
+        """
         # complicated way of finding and writing into folder name written by hiprmc
         ind = self.output.index(self.save_name)
         rmc_folder = './{}'.format(self.output[ind:].split("\n")[0])
