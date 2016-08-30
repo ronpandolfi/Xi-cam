cb26763fea5ac8c5f2bce1d28e7319f7a3dfc627
ronpandolfi <ronpandolfi@gmail.com>
2016-08-30 14:55:42 -0700

    Remove commented code.

--- xicam/plugins/batch/__init__.py
+++ xicam/plugins/batch/__init__.py
@@ -33,19 +33,7 @@ class plugin(base.plugin):
         self.centerwidget.layout().addWidget(self.fileslistwidget)
 
         self.rightwidget = None
-        # self.remeshOption = pTypes.SimpleParameter(type='bool', name='GIXS remeshing', value=False)
-        # self.integrateOption = pTypes.SimpleParameter(type='bool', name='Azimuthal integration', value=True)
-        # self.roiOption = pTypes.SimpleParameter(type='bool', name='Integrate last ROI', value=True)
-        # self.logOption = pTypes.SimpleParameter(type='bool', name='Log scale image', value=False)
-        # self.cakeOption = pTypes.SimpleParameter(type='bool', name='Cake (q/chi)', value=False)
-        # self.exportformat = pTypes.ListParameter(type='list', name='Image export format', value=0, values=['EDF (.edf)','TIFF (.tif)','JPEG (.jpg)'])
-        # self.processButton = pTypes.ActionParameter(name='Process')
-        # # self.abortButton = pTypes.ActionParameter(name='Abort')
-        # params = [self.remeshOption, self.cakeOption, self.integrateOption, self.roiOption, self.logOption, self.exportformat, self.processButton]
-        # paramgroup = Parameter.create(name='params', type='group', children=params)
-        # self.rightwidget.setParameters(paramgroup, showTop=False)
 
-        # self.processButton.sigActivated.connect(self.processfiles)
 
         super(plugin, self).__init__(*args, **kwargs)
 
@@ -59,41 +47,5 @@ class plugin(base.plugin):
             dimg = loader.loaddiffimage(path)
             self.workflowWidget.runWorkflow(dimg=dimg, rawdata=dimg.rawdata)
 
-            # xglobals.statusbar.showMessage('Processing item ' + str(paths.index(path)+1) + ' of ' + str(len(paths))+ '...')
-            # xglobals.app.processEvents()
-            #
-            # dimg = loader.diffimage(path)
-            #
-            # if self.remeshOption.value():
-            #     data = dimg.remesh
-            #     if not writer.writeimage(data if not self.logOption.value() else (np.log(data * (data > 0) + (data < 1))), path, suffix='_remeshed', ext=imageext):
-            #         break
-            #
-            # if self.cakeOption.value():
-            #     data = dimg.cake
-            #     if not writer.writeimage(data, path, suffix='_cake', ext=imageext):
-            #         break
-            #
-            # if self.integrateOption.value():
-            #     x, y, _, _ = dimg.integrate()
-            #     data = np.array([x, y])
-            #     if not writer.writearray(data, path, suffix=''):
-            #         break
-            #
-            # if self.roiOption.value():
-            #     msg.logMessage(('lastroi:',xglobals.lastroi),msg.DEBUG)
-            #     if xglobals.lastroi is not None:
-            #         # lastroi is a tuple with an ROI item and an imageitem (both are need to get a cut array)
-            #         cut = (xglobals.lastroi[0].getArrayRegion(np.ones_like(dimg.data), xglobals.lastroi[1])).T
-            #         x, y, _, _ = dimg.integrate(cut=cut)
-            #         data = np.array([x, y])
-            #         if not writer.writearray(data, path, suffix='_roi'):
-            #             break
-            #     else:
-            #         pass  # No ROI was defined, hm...
-            #
-            # if os.path.splitext(path)[-1] != imageext:
-            #     data = dimg.data
-            #     writer.writeimage(data if not self.logOption.value() else (np.log(data * (data > 0) + (data < 1))), path, ext=imageext)
 
         msg.showMessage('Ready...')
