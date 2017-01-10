e8912bdbe69af12fbc4a639b9d2450e8446d034f
ronpandolfi <ronpandolfi@gmail.com>
2017-01-10 14:12:59 -0800

    fix tar arguments

--- buildlinux.sh
+++ buildlinux.sh
@@ -3,4 +3,4 @@ source ~/virtualenv/python2.7_with_system_site_packages/bin/activate
 source venv/bin/activate
 sudo rm -rf build
 python buildexe.py build
-tar -zcvf Xi-cam.linux-x86_64-2.7 dist/exe.linux-x86_64-2.7
+tar -zcvf Xi-cam.linux-x86_64-2.7 build/exe.linux-x86_64-2.7
