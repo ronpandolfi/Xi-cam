48e69d177941d7af65486642dc21e87d0986d0ab
ronpandolfi <ronpandolfi@gmail.com>
2017-01-11 09:28:34 -0800

    add system site-packages to pythonpath on travis build

--- buildlinux.sh
+++ buildlinux.sh
@@ -2,6 +2,6 @@
 source ~/virtualenv/python2.7_with_system_site_packages/bin/activate #works on travis
 source venv/bin/activate # works on local
 sudo rm -rf build
-export PYTHONPATH=/usr/lib/python2.7/dist-packages/:$PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages/
 python buildexe.py build
 tar -zcvf Xi-cam.linux-x86_64-2.7 build/exe.linux-x86_64-2.7
