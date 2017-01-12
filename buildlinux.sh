88a727dd7d2bb7dc85d7d9dbe81c5af73118aca7
ronpandolfi <ronpandolfi@gmail.com>
2017-01-12 09:29:40 -0800

    add numpy location print

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-source ~/virtualenv/python2.7_with_system_site_packages/bin/activate #works on travis
+source ~/virtualenv/python2.7_with_system_site_packages/bin/activate # works on travis
 source venv/bin/activate # works on local
 sudo rm -rf build
-export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages/
+# export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages/
 echo $PYTHONPATH
 python -c "import numpy; print numpy.__file__"
 python buildexe.py build
