2347813cb2b1a0c05357fd240cbe66e1c7b3c6c4
ronpandolfi <ronpandolfi@gmail.com>
2017-01-12 10:27:24 -0800

    add system dist-packages to virtualenv on travis build

--- buildlinux.sh
+++ buildlinux.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-source ~/virtualenv/python2.7_with_system_site_packages/bin/activate # works on travis
-source venv/bin/activate # works on local
 sudo rm -rf build
 # export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages/
 echo $PYTHONPATH
+echo "/usr/lib/python2.7/dist-packages/" > ~/virtualenv/python2.7_with_system_site_packages/local/lib/python2.7/site-packages/global.pth
 python -c "import numpy; print numpy.__file__"
 python buildexe.py build
 tar -zcvf Xi-cam.linux-x86_64-2.7 build/exe.linux-x86_64-2.7
