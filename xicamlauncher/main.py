4104e02960772c5b0be59548a3359ebfab32e161
hparks <holdenparks@yahoo.com>
2017-01-13 15:17:58 -0800

    splash screen fixed

--- xicamlauncher/main.py
+++ xicamlauncher/main.py
@@ -27,7 +27,7 @@ def main():
     import xicam  # IMPORTANT! DO NOT REMOVE! Xicam must be loaded early to avoid graphical bugs on mac (?!)
     app=QtGui.QApplication(sys.argv)
 
-    pixmap = QtGui.QPixmap("splash.gif")
+    pixmap = QtGui.QPixmap("xicam/gui/splash.gif")
     print 'CWD:', os.getcwd()
     if True:  # Disable to bypass splashscreen for testing on windows
         splash = SplashScreen(pixmap, f=QtCore.Qt.WindowStaysOnTopHint | QtCore.Qt.SplashScreen)
