c0370038c38496993fe4b366558eb405a7113304
fang-ren <ren.fang.ren@gmail.com>
2017-03-08 16:21:44 -0800

    updated __init__.py

--- xicam/plugins/HiTp/__init__.py
+++ xicam/plugins/HiTp/__init__.py
@@ -15,11 +15,11 @@ def openfiles(filepaths):
 
 EZTest=base.EZplugin(name='HiTp',
                      toolbuttons=[],#('xicam/gui/icons_34.png',runtest)
-                     parameters=[{'name':'d_in_pixel','value':2500,'type':'float'},
-                                 {'name':'beam_center_x','value':1024,'type':'float'},
-                                 {'name':'beam_center_y','value':2500,'type':'float'},
-                                 {'name': 'rotation_rad', 'value': 4.7, 'type': 'float'},
-                                 {'name': 'tilt_rad', 'value': 0.5, 'type': 'float'},
+                     parameters=[{'name':'ddetect_dist_pix','value':2500,'type':'float'},
+                                 {'name':'bcenter_x_pix','value':1024,'type':'float'},
+                                 {'name':'bcenter_x_pix','value':2500,'type':'float'},
+                                 {'name': 'detect_tilt_alpha_rad', 'value': 4.7, 'type': 'float'},
+                                 {'name': 'detect_tilt_delta_rad', 'value': 0.5, 'type': 'float'},
                                  {'name': 'wavelength_A', 'value': 0.9762, 'type': 'float'},
                                  {'name': 'polarization', 'value': 0.95, 'type': 'float'},
                                  {'name': 'Imax_Iave_ratio_module', 'value': True, 'type': 'boolean'},
