c90f09a1f2b1877afeac763c957ae3202cdc664d
fang-ren <ren.fang.ren@gmail.com>
2017-05-10 14:25:16 -0700

    update indent for return csvpath

--- xicam/plugins/HiTp/__init__.py
+++ xicam/plugins/HiTp/__init__.py
@@ -19,43 +19,38 @@ def redrawfromCSV(csvpath):
 
 @threads.method(redrawfromCSV)
 def openfiles(filepaths):
-    '''
-
+    """
     Parameters
     ----------
     filepaths : list
         list of filepaths
+    """
+    # calibration
+    detect_dist_pix = HiTpPlugin.parameters.param('detect_dist_pix').value()
+    bcenter_x_pix = HiTpPlugin.parameters.param('bcenter_x_pix').value()
+    bcenter_y_pix = HiTpPlugin.parameters.param('bcenter_y_pix').value()
+    detect_tilt_alpha_rad = HiTpPlugin.parameters.param('detect_tilt_alpha_rad').value()
+    detect_tilt_beta_rad = HiTpPlugin.parameters.param('detect_tilt_delta_rad').value()
+    wavelength_A = HiTpPlugin.parameters.param('wavelength_A').value()
+    # first_scan = HiTpPlugin.parameters.param('first_scan').value()
+    # last_scan = HiTpPlugin.parameters.param('last_scan').value()
+    polarization = HiTpPlugin.parameters.param('polarization').value()
 
-    '''
-    for filepath in filepaths:
-        # # TODO: should this line be moved?
-        # HiTpPlugin.centerwidget.sigPlot.connect(HiTpPlugin.bottomwidget.plot)
+    # parameter
+    # first_scan = HiTpPlugin.parameters.param('first_scan').value()
+    # last_scan = HiTpPlugin.parameters.param('last_scan').value()
+    smpls_per_row = HiTpPlugin.parameters.param('smpls_per_row').value()
 
-        # calibration
-        detect_dist_pix = HiTpPlugin.parameters.param('detect_dist_pix').value()
-        bcenter_x_pix = HiTpPlugin.parameters.param('bcenter_x_pix').value()
-        bcenter_y_pix = HiTpPlugin.parameters.param('bcenter_y_pix').value()
-        detect_tilt_alpha_rad = HiTpPlugin.parameters.param('detect_tilt_alpha_rad').value()
-        detect_tilt_beta_rad = HiTpPlugin.parameters.param('detect_tilt_delta_rad').value()
-        wavelength_A = HiTpPlugin.parameters.param('wavelength_A').value()
-        #first_scan = HiTpPlugin.parameters.param('first_scan').value()
-        #last_scan = HiTpPlugin.parameters.param('last_scan').value()
-        polarization = HiTpPlugin.parameters.param('polarization').value()
+    # modules
+    Imax_Iave_ratio_module = HiTpPlugin.parameters.param('Imax_Iave_ratio_module').value()
+    texture_module = HiTpPlugin.parameters.param('texture_module').value()
+    signal_to_noise_module = HiTpPlugin.parameters.param('signal_to_noise_module').value()
+    neighbor_distance_module = HiTpPlugin.parameters.param('neighbor_distance_module').value()
+    add_feature_to_csv_module = HiTpPlugin.parameters.param('add_feature_to_csv_module').value()
+    background_subtract_module = HiTpPlugin.parameters.param('background_subtraction_module').value()
+    peak_fitting_module = HiTpPlugin.parameters.param('peak_fitting_module').value()
 
-        # parameter
-        # first_scan = HiTpPlugin.parameters.param('first_scan').value()
-        # last_scan = HiTpPlugin.parameters.param('last_scan').value()
-        smpls_per_row = HiTpPlugin.parameters.param('smpls_per_row').value()
-
-        # modules
-        Imax_Iave_ratio_module = HiTpPlugin.parameters.param('Imax_Iave_ratio_module').value()
-        texture_module = HiTpPlugin.parameters.param('texture_module').value()
-        signal_to_noise_module = HiTpPlugin.parameters.param('signal_to_noise_module').value()
-        neighbor_distance_module = HiTpPlugin.parameters.param('neighbor_distance_module').value()
-        add_feature_to_csv_module = HiTpPlugin.parameters.param('add_feature_to_csv_module').value()
-        background_subtract_module = HiTpPlugin.parameters.param('background_subtraction_module').value()
-        peak_fitting_module = HiTpPlugin.parameters.param('peak_fitting_module').value()
-
+    for filepath in sorted(filepaths):
         run(filepath, detect_dist_pix, detect_tilt_alpha_rad, detect_tilt_beta_rad, wavelength_A,
             bcenter_x_pix, bcenter_y_pix,
             polarization, smpls_per_row,
@@ -67,15 +62,10 @@ def openfiles(filepaths):
             background_subtract_module,
             peak_fitting_module)
 
-
-        csvpath = ''
-
-        ## TODO: Is it possible to point to the csv file?
-        # dummy way to choose a csv file for now
         folder_path, imageFilename = os.path.split(os.path.abspath(filepath))
         csvpath = os.path.join(folder_path, 'Processed//attributes.csv')
+    return csvpath
 
-        return csvpath
 
 HiTpPlugin=base.EZplugin(name='HiTp',
                      toolbuttons=[],#('xicam/gui/icons_34.png',runtest)
