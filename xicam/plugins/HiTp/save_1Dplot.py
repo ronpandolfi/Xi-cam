e372d978d564c14c6ce7e58e63cb4ea04e617012
fang-ren <ren.fang.ren@gmail.com>
2017-07-07 14:40:00 -0700

    fixed the multithred plotting problem

--- xicam/plugins/HiTp/save_1Dplot.py
+++ xicam/plugins/HiTp/save_1Dplot.py
@@ -12,13 +12,11 @@ import os.path
 
 def save_1Dplot(Qlist, IntAve, peaks, imageFilename, save_path):
     # generate a column average image
-    plt.figure(2)
-    plt.title('Column average')
-    plt.plot(Qlist, IntAve)
-    plt.xlabel('Q')
-    plt.ylabel('Intensity')
-    # plt.xlim((0.7, 6.4))
-
-    plt.savefig(os.path.join(save_path, imageFilename[:-4]+'_1D'))
-    
-    plt.close()
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.set_title('Column average')
+    ax.plot(Qlist, IntAve)
+    ax.set_xlabel('Q')
+    ax.set_ylabel('Intensity')
+    ax.set_xlim((0.7, 6.4))
+    fig.savefig(os.path.join(save_path, imageFilename[:-4]+'_1D'))
