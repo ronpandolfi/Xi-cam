8f39c58b35d507f099432c227b7b85701f864467
Lenson Pellouchoud <lensonp@gmail.com>
2017-08-28 23:14:35 -0700

    wip: some connections good, others bad, stack widget funky but no longer segfaults

--- xicam/plugins/batch/__init__.py
+++ xicam/plugins/batch/__init__.py
@@ -50,7 +50,7 @@ class BatchPlugin(base.plugin):
         self.paw.activate_op('EXECUTION.BATCH.BatchFromFiles')
 
         self.build_ui()
-        self.centerwidget = self.viewer_tabs
+        self.centerwidget = self.viewer_stack
         self.rightwidget = self.wf_control
         self.bottomwidget = self.batch_control
 
@@ -65,7 +65,7 @@ class BatchPlugin(base.plugin):
         self.remove_files_button = QtGui.QPushButton('Remove selected files')
         self.remove_files_button.clicked.connect(self.rm_files)
 
-        self.viewer_tabs = QtGui.QStackedWidget()
+        self.viewer_stack = QtGui.QStackedWidget()
         self.wf_control = pt.ParameterTree()
         self.wf_control.setHeaderLabels(['Operation','Settings'])
         self.wf_control.itemSelectionChanged.connect(self.itemSelectionChanged)
@@ -83,11 +83,14 @@ class BatchPlugin(base.plugin):
     #    pass
     #    print args
     #    print kwargs
-        item=self.wf_control.selectedItems()[0]
-        while item.depth>0: item=item.param.parent().items.keys()[0] # ascend until at 'operation' depth
-        if item.param.name()=='&Run': return # do nothing if its the run button
-        selected_tag = item.param.name()
-        self.set_visualizer(selected_tag,True)
+    #    if item.param.name()=='&Run': return # do nothing if its the run button
+        sel = self.wf_control.selectedItems()
+        if any(sel):
+            itm = sel[0]
+            while itm.depth>0: itm=itm.param.parent().items.keys()[0] # ascend until at 'operation' depth
+            itm_tag = itm.param.name()
+            if itm_tag in self.ops.keys():
+                self.set_visualizer(itm_tag,True)
 
     def rm_files(self):
         itms = self.batch_list.selectedItems()
@@ -105,75 +108,80 @@ class BatchPlugin(base.plugin):
         self.paw.set_input('batch','workflow',self._wfname)
         self.paw.set_input('batch','input_name','image_path')
 
-        # Set up the rest of the workflow
+        # Set up the read-and-integrate workflow
         self.paw.select_wf(self._wfname)
+        # Create a root parameter for the ParameterTree
         root_param = pt.types.GroupParameter(name='root')
         for op_tag,op_uri in self.ops.items():
-            #p = pt.types.SimpleParameter(name=op_tag, type='bool',showTop=False, value=True, expanded=False)
-            #root.addChild(p)
-            #    p.addChild(pt.types.SimpleParameter(name=nm,type=tp,value=val))
-            # TODO: add operation parameters as children so they are editable
-            # If an op has children, it becomes expandable
-            #p.addChild(pt.types.SimpleParameter(name='Example',type='int',value='42'))
-
             # Add the op to the workflow
             self.paw.add_op(op_tag,op_uri,self._wfname)
-
-            # Set up the inputs....
+            # Set up the inputs
             self._default_op_setup(op_tag)
+            # Connect the ParameterTree
             self._param_setup(root_param,op_tag)
-
+            # Connect the viewer
+            self.paw.get_wf(self._wfname).opFinished.connect( self.update_visuals )
+            # TODO: Connect paws opChanged signal to update parameter tree
         run_wf_button=pt.types.ActionParameter(name='&Run')
         run_wf_button.sigActivated.connect(self.run_wf)
         root_param.addChild(run_wf_button)
-
         self.wf_control.setParameters(root_param,showTop=False)
 
     def _param_setup(self,root_param,op_tag):
-        # TODO: Connect op input changed signals to update parameter tree
-        # TODO: Connect operation Parameter changed signal to paw.activate_op
-        op = self.paw.get_op(op_tag)
         # op Parameter 
         default_enabled = True
         if op_tag in ['Output CSV','Output Image']:
             default_enabled = False 
             self.paw.disable_op(op_tag,self._wfname)
         p = pt.types.SimpleParameter(name=op_tag, type='bool',showTop=False, value=default_enabled, expanded=False)
-        p.sigValueChanged.connect( self._set_op_enabled )
+        p.sigValueChanged.connect( partial(self._set_op_enabled,op_tag) )
         # child Parameters
         if op_tag == 'Integrate to 1d':
-            pc = pt.types.SimpleParameter(name='number of q-points',type='int',value=op.inputs['npt'])
+            pc = pt.types.SimpleParameter(name='number of q-points',
+            type='int',value=self.paw.get_input_setting(op_tag,'npt'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'npt') )  
             p.addChild(pc)
-            pc = pt.types.SimpleParameter(name='polarization factor',type='float',value=op.inputs['polarization_factor'])
+            pc = pt.types.SimpleParameter(name='polarization factor',
+            type='float',value=self.paw.get_input_setting(op_tag,'polarization_factor'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'polarization_factor') )  
             p.addChild(pc)
         elif op_tag == 'Integrate to 2d':
-            pc = pt.types.SimpleParameter(name='number of q-points',type='int',value=op.inputs['npt_rad'])
+            pc = pt.types.SimpleParameter(name='number of q-points',
+            type='int',value=self.paw.get_input_setting(op_tag,'npt_rad'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'npt_rad') )  
             p.addChild(pc)
-            pc = pt.types.SimpleParameter(name='number of chi-points',type='int',value=op.inputs['npt_azim'])
+            pc = pt.types.SimpleParameter(name='number of chi-points',
+            type='int',value=self.paw.get_input_setting(op_tag,'npt_azim'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'npt_azim') )  
             p.addChild(pc)
-            pc = pt.types.SimpleParameter(name='polarization factor',type='float',value=op.inputs['polarization_factor'])
+            pc = pt.types.SimpleParameter(name='polarization factor',
+            type='float',value=self.paw.get_input_setting(op_tag,'polarization_factor'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'polarization_factor') )  
             p.addChild(pc)
         elif op_tag in ['Output CSV','Output Image']:
-            # TODO: think of a way to include parameters in the tree,
+            # TODO: think of a way to include workflow items in the parametertree,
             # without interfering with the workflow routing
             # TODO: add a browse button for dir_path 
-            #pc = pt.types.SimpleParameter(name='directory path',type='str',value=op.inputs['dir_path'])
+            #pc = pt.types.SimpleParameter(name='directory path',
+            #type='str',value=self.paw.get_input_setting(op_tag,'dir_path'))
             #pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'dir_path') )  
             #p.addChild(pc)
-            #pc = pt.types.SimpleParameter(name='file name',type='str',value=op.inputs['filename'])
+            #pc = pt.types.SimpleParameter(name='file name',
+            #type='str',value=self.paw.get_input_setting(op_tag,'filename'))
             #pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'filename') )  
             #p.addChild(pc)
-            pc = pt.types.SimpleParameter(name='file tag',type='str',value=op.inputs['filetag'])
+            pc = pt.types.SimpleParameter(name='file tag',
+            type='str',value=self.paw.get_input_setting(op_tag,'filetag'))
             pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'filetag') )  
             p.addChild(pc)
             if op_tag == 'Output Image':
-                pc = pt.types.SimpleParameter(name='extension',type='str',value=op.inputs['ext'])
+                pc = pt.types.SimpleParameter(name='extension',
+                type='str',value=self.paw.get_input_setting(op_tag,'ext'))
                 pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'ext') )  
+                p.addChild(pc)
+                pc = pt.types.SimpleParameter(name='overwrite',
+                type='bool',value=self.paw.get_input_setting(op_tag,'overwrite'))
+                pc.sigValueChanged.connect( partial(self._set_parameter,op_tag,'overwrite') )  
                 p.addChild(pc)
         root_param.addChild(p)
 
@@ -208,6 +216,7 @@ class BatchPlugin(base.plugin):
             self.paw.set_input(op_tag,'dir_path','Read Image.outputs.dir_path','workflow item')
             self.paw.set_input(op_tag,'filename','Read Image.outputs.filename','workflow item')
             self.paw.set_input(op_tag,'filetag','_q_chi_I')
+            self.paw.set_input(op_tag,'overwrite',True)
             self.paw.set_input(op_tag,'ext','.edf')
 
     def toggle_enabled(self,op_tag,state):
@@ -219,28 +228,26 @@ class BatchPlugin(base.plugin):
     def edit_op(self,op_tag):
         pass
 
-    def set_visualizer(self,op_tag,state):
-        if not state==0:
-            # Find, create, or otherwise open the widget
-            if not op_tag in self.output_widgets.keys():
-                widg = self.make_widget(op_tag)
-                if widg is not None:
-                    self.output_widgets[op_tag] = widg
-            else:
-                # The user closed the tab
-                # instead of un-checking the visualizer box,
-                # so the widget should still be in self.output_widgets
-                widg = self.output_widgets[op_tag]
-            if self.viewer_tabs.indexOf(widg) == -1:
-                tab_idx = self.viewer_tabs.addWidget(widg)
-            self.viewer_tabs.setCurrentWidget(widg)
-        else:
-            widg = self.output_widgets.pop(op_tag)
+    def set_visualizer(self,op_tag,state=False):
+        # Find, create, or otherwise open the widget
+        if op_tag in self.output_widgets.keys():
+            widg = self.output_widgets[op_tag]
+            if not self.viewer_stack.indexOf(widg) == -1:
+                # Get rid of old widget
+                self.viewer_stack.removeWidget(widg)
+        if state:
+            # Create new widget
+            widg = self.make_widget(op_tag)
             if widg is not None:
-                tab_idx = self.viewer_tabs.indexOf(widg)
-                widg.close()
-                if not tab_idx == -1:
-                    self.viewer_tabs.removeTab(tab_idx)
+                self.output_widgets[op_tag] = widg
+                self.viewer_stack.addWidget(widg)
+        #else:
+        #    widg = self.output_widgets.pop(op_tag)
+        #    if widg is not None:
+        #        tab_idx = self.viewer_stack.indexOf(widg)
+        #        widg.close()
+        #        if not tab_idx == -1:
+        #            self.viewer_stack.removeTab(tab_idx)
 
     def make_widget(self,op_tag):
         if op_tag == 'Read Image':
@@ -254,7 +261,7 @@ class BatchPlugin(base.plugin):
         elif op_tag == 'log(I) 2d':
             output_data = self.paw.get_output(op_tag,'logx',self._wfname)
         elif op_tag == 'Output CSV':
-            output_data = self.paw.get_output(op_tag,'csv_path',self._wfname)
+            output_data = self.paw.get_output(op_tag,'file_path',self._wfname)
         elif op_tag == 'Output Image':
             output_data = self.paw.get_output(op_tag,'file_path',self._wfname)
         # Form a widget from the output data 
@@ -270,13 +277,13 @@ class BatchPlugin(base.plugin):
             p = self.batch_list.item(r).text()
             file_list.append(p)
         self.paw.set_input('batch','file_list',file_list)
+        # TODO: Move this off the main thread.
         self.paw.execute()
-        self.update_visuals()
+        #self.update_visuals()
 
-    def update_visuals(self):
-        for widg in self.output_widgets:
-            if isinstance(widg,QtGui.QWidget):
-                widg.repaint()
+    def update_visuals(self,opname):
+        if opname in self.output_widgets:
+            self.output_widgets[opname].repaint()
 
     def openfiles(self, files, operation=None, operationname=None):
         self.batch_list.addItems(files)
