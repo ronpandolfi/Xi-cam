399fae239b18eb78f83bca6076faab94f95eec0a
Ron Pandolfi <ronpandolfi@gmail.com>
2017-09-13 07:59:24 -0700

    Merge

--- pipeline/publish/__init__.py
+++ pipeline/publish/__init__.py
@@ -1,4 +1,4 @@
-
+import datetime
 
 class PublishablePayload(dict):
     def __init__(self, title, source_name, data_contact, data_contributor, links, **kwargs):
@@ -32,12 +32,43 @@ class PublishablePayload(dict):
         for prop in optionalkeys:
             self[prop] = kwargs.get(prop,None)
 
+        self['title'] = title
+        self['source_name'] = source_name
+        self['data_contact'] = data_contact
+        self['data_contributor'] = data_contributor
+        self['links'] = links
+
 
 class CITPayload(PublishablePayload):
     pass
 
 class MDFPayload(PublishablePayload):
-    pass
+    def __init__(self):
+        super(MDFPayload, self).__init__()
+
+    @property
+    def metapayload(self):
+        dataset = {
+            "mdf": {
+                "title": self.title,
+                "acl": [],                                              # TODO: what goes here?
+                "source_name": self.source_name,
+                "citation": self.citation,
+                "links": self.links,
+                "data_contact": dict(self.data_contact),
+                "data_contributor": dict(self.data_contributor),
+                "ingest_date": datetime.datetime.now().strftime('%b %d, %Y'),                           # TODO: shouldn't this be implied?
+                "metadata_version": "1.1",
+                "mdf_id": "1",
+                "resource_type": "dataset"
+            },
+            "dc": {},
+            "misc": {}
+        }
+
+        return dataset
+
+
 
 class MCPayload(PublishablePayload):
     pass
