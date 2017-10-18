bfc26cd67ebb8d47a3e32510fe95b9c18cf7cb90
dkumar <dkumar@lbl.gov>
2017-10-18 19:11:01 +0000

    future interface to msmcam

---
+++ xicam/plugins/msmcam/workflow.py
@@ -0,0 +1,29 @@
+__author__ = "Dinesh Kumar"
+__copyright__ = "Copyright 2016, CAMERA, LBL, ALS"
+__credits__ = ["Ronald J Pandolfi", "Dinesh Kumar", "Singanallur Venkatakrishnan", "Luis Luque",
+               "Holden Parks", "Alexander Hexemer"]
+__license__ = ""
+__version__ = "0.0.1"
+__maintainer__ = "Ronald J Pandolfi"
+__email__ = "ronpandolfi@lbl.gov"
+__status__ = "Alpha"
+
+
+import numpy as np
+import msmcam
+
+class Workflow(object):
+    def __init__(self):
+
+    def load_mask(self):
+
+    def apply_mask(self):
+        
+        
+    def apply_filter(self):
+
+    def run_segementation(self):
+        
+
+    @property
+    def currentFrame(self):
